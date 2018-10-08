c99123e19fff0b1f3f3671be657709d585e4534c
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-10-08 08:02:24 +0200

    enums

---
+++ src/tutorial/12-enums.kt
@@ -0,0 +1,34 @@
+package tutorial
+
+/**
+ * Enum classes: https://kotlinlang.org/docs/reference/enum-classes.html
+ *
+ * @author Lukasz Frankowski
+ */
+
+// enum classes are basically the same as in java
+
+// basic enum
+enum class Status {
+    OPENED,
+    CLOSED
+}
+
+// extended enum
+enum class ColoredStatus(val color: Int): Greetable { // can have constructor and implement interfaces
+
+    OPENED(1) {
+
+        override fun hello() { // interface method implementation
+            println("hello, ${sysName()}")
+        }
+
+        override fun sysName(): String { // internal method implementation
+            return "opened"
+        }
+    };
+
+    protected abstract fun sysName(): String // can have internal methods
+    
+}
+
