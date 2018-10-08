b93dcebb30fe100ddb4d07f80e5e45f2e350e759
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-10-08 07:53:06 +0200

    nested classes

---
+++ src/tutorial/11-nested-classes.kt
@@ -0,0 +1,66 @@
+package tutorial
+
+/**
+ * Nested classes: https://kotlinlang.org/docs/reference/nested-classes.html
+ *
+ * @author Lukasz Frankowski
+ */
+
+interface Greetable {
+    fun hello()
+}
+
+class Outer {
+
+    var greetingsCount: Int = 0;
+
+    class Nested: Greetable { // default nested class doesn't have connection to outer class instance (java equivalent is nested static class)
+        override fun hello() {
+            println("hello")
+            // greetingsCount++; // Error:(16, 13) Kotlin: Unresolved reference: x
+        }
+    }
+
+    inner class Inner: Greetable { // inner class is connected with outer class instance
+        override fun hello() {
+            println("hello")
+            greetingsCount++; // can use outer class property
+
+            this@Outer.greetingsCount; // example of qualified outer class "this"
+        }
+    }
+
+    fun hello(g: Greetable) {
+        g.hello()
+    }
+
+    fun exampleHello() {
+        hello(object: Greetable { // anonymous class can be created with "object" clause
+            
+            override fun hello() {
+                println("hi")
+            }
+
+        })
+    }
+    
+}
+
+fun main(args: Array<String>) {
+
+    println("nested class")
+    run {
+        Outer.Nested().hello() // it's only name qualifier
+    }
+
+    println("\ninner class")
+    run {
+        Outer().Inner().hello() // outer class instance is required to instantiate inner class
+    }
+
+    println("\nanonymous class")
+    run {
+        Outer().exampleHello()
+    }
+
+}
